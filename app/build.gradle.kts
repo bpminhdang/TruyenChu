9c7f2d113bc6a22f470fc9d1c6a50d06a1a8ce37
Minh Dang <minhdang16012003@gmail.com>
2024-01-05 09:36:53 +0700

    Đăng nhập với google: vẫn chưa đăng nhập được

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
 }
 
 dependencies {
-    implementation ("com.facebook.android:facebook-android-sdk:latest.release")
+    implementation ("com.facebook.android:facebook-android-sdk:16.0.0")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
     implementation("androidx.preference:preference:1.2.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
@@ -42,16 +42,15 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    //implementation("com.google.firebase:firebase-auth:22.2.0")
+    implementation("com.google.firebase:firebase-auth:22.3.0")
     implementation("com.google.firebase:firebase-database:20.3.0")
     implementation("com.google.firebase:firebase-storage:20.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    // Import the BoM for the Firebase platform
-    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
 
-    // Add the dependency for the Firebase Authentication library
-    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
     implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
+
 }
--- app/src/main/java/com/example/truyenchu/Login.java
+++ app/src/main/java/com/example/truyenchu/Login.java
@@ -1,6 +1,7 @@
 package com.example.truyenchu;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
@@ -20,6 +21,12 @@ import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
@@ -29,7 +36,10 @@ import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.facebook.FacebookSdk;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.HashMap;
 import java.util.Objects;
 
 
@@ -39,11 +49,17 @@ public class Login extends AppCompatActivity
     TextInputEditText editTextPassword;
     Button buttonLogin;
     FirebaseAuth mAuth;
+    FirebaseDatabase database;
+    GoogleSignInClient mGoogleSignInClient;
     LoginButton loginButton;
     ProgressBar progressBar;
     TextView textView;
 
     CallbackManager mCallbackManager;
+
+    Button googleAuth;
+    int RC_SIGN_IN = 20;
+
 
     @Override
     public void onStart() {
@@ -145,7 +161,74 @@ public class Login extends AppCompatActivity
             public void onError(FacebookException error) {
             }
         });
+
+
+        //Google Auth
+        googleAuth = findViewById(R.id.btnGoogleAuth);
+        database = FirebaseDatabase.getInstance();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail().build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        googleAuth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                googleSignIn();
+            }
+        });
+
     }
+
+    private void googleSignIn() {
+        Intent intent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(intent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==RC_SIGN_IN)
+        {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                fireBaseAuth(account.getIdToken());
+            }
+            catch (Exception e){
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
+    private void fireBaseAuth(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>()
+                 {
+                      @Override
+                         public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                HashMap<String,Object> map = new HashMap<>();
+                                map.put("id", user.getUid());
+                                map.put("name", user.getDisplayName());
+                                map.put("profile",user.getPhotoUrl().toString());
+                                database.getReference().child("users").child(user.getUid()).setValue(map);
+                                updateUI(user);
+                            }
+                            else{
+                                Toast.makeText(Login.this, "Đã xảy ra lỗi", Toast.LENGTH_SHORT).show();
+                                updateUI(null);
+                            }
+                 }
+        });
+    }
+
     private void handleFacebookAccessToken(AccessToken token) {
         AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
         mAuth.signInWithCredential(credential)
@@ -176,4 +259,5 @@ public class Login extends AppCompatActivity
                 Toast.makeText(this, "Vui lòng đăng nhập để tiếp tục", Toast.LENGTH_SHORT).show();
             }
     }
+
 }
