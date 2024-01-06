c58a859bd226a5e48145b986621cf1a450ad9c92
shielbabi <21520221@gm.uit.edu.vn>
2024-01-06 13:53:56 +0700

    update ui

--- app/src/main/java/com/example/truyenchu/features/StoryDescriptionFragment.java
+++ app/src/main/java/com/example/truyenchu/features/StoryDescriptionFragment.java
@@ -16,13 +16,18 @@ import com.bumptech.glide.Glide;
 import com.example.truyenchu.R;
 import com.example.truyenchu._class.StoryClass;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link StoryDescriptionFragment#newInstance} factory method to
- * create an instance of this fragment.*/
+ * create an instance of this fragment.
+ */
 
-public class StoryDescriptionFragment extends Fragment
-{
+public class StoryDescriptionFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -32,8 +37,8 @@ public class StoryDescriptionFragment extends Fragment
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-    public StoryDescriptionFragment()
-    {
+
+    public StoryDescriptionFragment() {
         // Required empty public constructor
     }
 
@@ -43,11 +48,11 @@ public class StoryDescriptionFragment extends Fragment
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment StoryDescriptionFragment.*/
+     * @return A new instance of fragment StoryDescriptionFragment.
+     */
 
     // TODO: Rename and change types and number of parameters
-    public static StoryDescriptionFragment newInstance(String param1, String param2)
-    {
+    public static StoryDescriptionFragment newInstance(String param1, String param2) {
         StoryDescriptionFragment fragment = new StoryDescriptionFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
@@ -57,11 +62,9 @@ public class StoryDescriptionFragment extends Fragment
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null)
-        {
+        if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
@@ -69,8 +72,7 @@ public class StoryDescriptionFragment extends Fragment
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState)
-    {
+                             Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_story_description, container, false);
 
@@ -89,13 +91,17 @@ public class StoryDescriptionFragment extends Fragment
 
 
         Bundle bundle = getArguments();
-        if (bundle != null)
-        {
+        if (bundle != null) {
             StoryClass receivedStory = (StoryClass) bundle.getSerializable("receivedStory");
             StoryClass.SetText(tvNameDes, receivedStory.getName());
             StoryClass.SetImage(requireContext(), receivedStory.getUri(), ivPicture);
             StoryClass.SetText(tvAuthor, receivedStory.getAuthor());
-            StoryClass.SetText(tvStatus, receivedStory.getStatus() + " - " + receivedStory.getTime() + " - " + receivedStory.getUpdateTime());
+            String receivedDateString = receivedStory.getUpdateTime(); // Chuỗi ngày từ receivedStory
+            String stt = receivedStory.getStatus();
+            if ("Đang cập nhật".equals(stt)) {
+                stt = "Đang ra - ";
+            }
+            StoryClass.SetText(tvStatus, stt + formattedDateString(receivedDateString));
             StoryClass.SetText(tvNumChapter, String.valueOf(receivedStory.getNumberOfChapter()));
             StoryClass.SetText(tvWatching, "Đang xem: " + "40");
             StoryClass.SetText(tvLiked, "100");
@@ -105,10 +111,21 @@ public class StoryDescriptionFragment extends Fragment
 
 
         }
-        ivBack.setOnClickListener(v-> requireActivity().onBackPressed());
-
-
+        ivBack.setOnClickListener(v -> requireActivity().onBackPressed());
 
         return view;
+    }
+
+    private String formattedDateString(String dateString) {
+        try {
+            SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+            SimpleDateFormat outputFormat = new SimpleDateFormat("'ngày' d 'th' M, yyyy", Locale.getDefault());
+
+            Date date = inputFormat.parse(dateString);
+            return outputFormat.format(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return dateString; // Trả về chuỗi ban đầu nếu có lỗi
+        }
     }
 }
